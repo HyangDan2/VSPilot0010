dbccbecd1ac78885c1f26e9bc262b8364f2903d1
HyangDan2 <49368735+HyangDan2@users.noreply.github.com>
2025-08-08 13:15:52 +0900

    Create VideoLabel.py

---
+++ Sources/VideoLabel.py
@@ -0,0 +1,23 @@
+from PySide6.QtGui import QPainter, QColor, QFont
+from PySide6.QtWidgets import QLabel
+
+class VideoLabel(QLabel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.metadata_text = ""
+        self.show_metadata = True 
+
+    def set_metadata(self, text):
+        self.metadata_text = text
+        self.update()
+
+    def paintEvent(self,event):
+        super().paintEvent(event)
+
+        if self.show_metadata and self.metadata_text:
+            painter = QPainter(self)
+            painter.setRenderHint(QPainter.Antialiasing)
+            painter.setPen(QColor(255,255,255))
+            painter.setFont(QFont("Arial", 14, QFont.Weight.Bold))
+            painter.drawText(10, 25, self.metadata_text)
+            painter.end()
